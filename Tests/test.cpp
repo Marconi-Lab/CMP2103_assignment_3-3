5e3b00c080903ead6dd3f03fed8017f30219276b
Paul Mugume Okwija <okwijapaul@gmail.com>
2022-11-21 21:30:05 +0300

    Update test.cpp

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -3,9 +3,12 @@
 #include "../main.h"
 
 double value_t1 = 1234.56;
+double value_t2 = 1789895.61;
 
 TEST_CASE( "Tests_1", "Tests" ) {
     REQUIRE( is_valid(value_t1) == 0);
 }
 
-
+TEST_CASE( "Tests_2", "Tests" ) {
+    REQUIRE( is_valid(value_t2) == 1);
+}
